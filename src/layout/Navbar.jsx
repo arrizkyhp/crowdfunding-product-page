41cf35dc2c2a0e39cc9f15457289f24a33b17ccc
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-02-07 21:41:06 +0700

    update slicing info & progress bar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from 'assets/images/logo.svg';
 
 const Navbar = () => {
-  return <nav className="container mx-auto max-w-7xl px-3 mt-8 flex flex-row justify-between items-center z-10">
+  return <nav className="container mx-auto max-w-7xl px-3 mt-10 flex flex-row justify-between items-center z-10">
     <img src={logo} alt="" />
     <ul className="flex flex-row gap-x-6 text-white">
         <li><a href="/" className='hover:underline hover:underline-offset-1'>About</a></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import iconBookmark from 'assets/images/icon-bookmark.svg'
 import { FaBookmark } from 'react-icons/fa'
 
 export const Home = () => {
-  return <main className="container flex flex-col items-center  max-w-4xl mt-60 ">
-      <div className="bg-white relative  rounded-lg border border-slate-200 items-center flex flex-col py-16 w-full px-16">
+  return <main className="container flex flex-col items-center  max-w-4xl mt-56 ">
+      <div className="bg-white relative  rounded-lg border border-slate-200 items-center flex flex-col py-16 w-full px-16 mb-8">
           <img src={logoMastercraft} alt="logo m" className='absolute -top-7 w-16' />
           <h1 className='font-bold text-4xl mb-5'>Mastercraft Bamboo Monitor Riser</h1>
           <p className='text-xl text-grey-custom-400 mb-10'>A beautiful & handcrafted monitor stand to reduce neck and eye strain.</p>
@@ -17,6 +17,25 @@ export const Home = () => {
                   <p className='font-bold text-grey-custom-500 group-hover:opacity-80 transition ease-in-out duration-300 text-lg py-4 pr-5'>Bookmark</p>
               </a>
           </div>
+      </div>
+      <div className="bg-white relative  rounded-lg border border-slate-200 items-center flex flex-col py-16 w-full px-16 mb-8">
+         <div className="grid grid-cols-3 w-full justify-evenly gap-20 mb-8">
+          <div className="flex flex-col border-r gap-2 border-gray-300 border-solid">
+            <p className='text-4xl font-bold'>$89,914</p>
+            <p className='text-base text-gray-500'>of $100,000 backed</p>
+          </div>
+          <div className="flex flex-col border-r gap-2 border-gray-300 border-solid">
+            <p className='text-4xl font-bold'>5,007</p>
+            <p className='text-base text-gray-500'>total backers</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className='text-4xl font-bold'>56</p>
+            <p className='text-base text-gray-500'>days left</p>
+          </div>
+         </div>
+         <div className="bg-gray-100 w-full h-4 rounded-full">
+            <div className="bg-blue-button-primary w-4/5 h-full rounded-full"></div>
+         </div>
       </div>
   </main>;
 };
