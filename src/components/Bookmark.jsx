3c88d337a82054c363586419b1cf23eec392b0ad
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-04-10 11:37:49 +0700

    Update fix HTML  Validations #3 (bookmark only)

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -14,23 +14,23 @@ const Bookmark = (props) => {
 
     if(props.isBookmark) {
         return (
-            <button type='button' onClick={handleBookmark} className="group flex flex-row gap-3 bg-grey-custom-200  rounded-full">
+            <div role="button" aria-pressed="false" onClick={handleBookmark} className="group flex flex-row gap-3 bg-grey-custom-200  rounded-full">
                   <span className="bg-blue-button-primary text-grey-custom-300 transition ease-in-out duration-300 group-hover:bg-blue-button-hover w-14 h-14 rounded-full flex justify-center items-center">
                       <FaBookmark className='text-white'/>
                   </span>
                   <p className='font-bold text-blue-button-primary group-hover:text-blue-button-hover  group-hover:opacity-80 transition ease-in-out duration-300 text-lg py-4 pr-5 hidden md:inline-block'>Bookmarked</p>
-            </button>
+            </div>
         )
     }
 
     if(props.isNotBookmark) {
         return (
-            <button type='button' onClick={handleBookmark} className="group flex flex-row gap-3 bg-grey-custom-200  rounded-full">
+            <div role='button' aria-pressed="false" onClick={handleBookmark} className="group flex flex-row gap-3 bg-grey-custom-200  rounded-full">
                 <span className="bg-grey-custom-600 text-grey-custom-300 transition ease-in-out duration-300 group-hover:bg-grey-custom-300 w-14 h-14 rounded-full flex justify-center items-center">
                     <FaBookmark className='text-slate-300'/>
                 </span>
                 <p className='font-bold text-grey-custom-500 group-hover:opacity-80 transition ease-in-out duration-300 text-lg py-4 pr-5 hidden md:inline-block'>Bookmark</p>
-            </button>
+            </div>
         )
     } 
 }
