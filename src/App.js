7471ff9b9a4b1ac43c5e3141e484e2a36435929f
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-04-10 10:30:35 +0700

    Update Routing App.js #2

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@ import { BookmarkProvider } from "context/bookmark/BookmarkContext";
 import { DataProvider } from "context/data/DataContext";
 import { ModalProvider } from "context/modal/ModalContext";
 import { NavbarProvider } from "context/navbar/NavbarContext";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import "./assets/css/style.css"
 import Navbar from "./layout/Navbar";
 import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Router>
       <NavbarProvider>
         <DataProvider>
           <ModalProvider>
             <BookmarkProvider>
-                <div className="flex flex-col justify-between font-commisioner">
-                  <div className="flex flex-col relative items-center bg-[url('assets/images/image-hero-mobile.jpg')] md:bg-[url('assets/images/image-hero-desktop.jpg')]  max-h-[25rem] bg-no-repeat md:h-96 w-full bg-cover">
-                    <Navbar />
-                    <Routes>
-                      <Route exact path="/" element={<Home />} />
-                    </Routes>
+              <BrowserRouter>
+                  <div className="flex flex-col justify-between font-commisioner">
+                    <div className="flex flex-col relative items-center bg-[url('assets/images/image-hero-mobile.jpg')] md:bg-[url('assets/images/image-hero-desktop.jpg')]  max-h-[25rem] bg-no-repeat md:h-96 w-full bg-cover">
+                      <Navbar />
+                        <Home />
+                    </div>
                   </div>
-                </div>
+                </BrowserRouter>
             </BookmarkProvider>
           </ModalProvider>
         </DataProvider>
       </NavbarProvider>
-    </Router>
   );
 }
 
